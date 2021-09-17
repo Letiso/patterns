e8ae92718db622b07a2c1abceeafbde8a611cb2d
Letiso <letisodianta@gmail.com>
2021-09-18 01:47:27 +0300

    Base prototype npc factory implemented

--- Creational/prototype.py
+++ Creational/prototype.py
@@ -105,18 +105,34 @@ class NPC(NPCPrototype, NPCConfig):
 
     def __str__(self):
         return f"\n{'*' * 100}\n" \
-                f"Race: {self._race}\n" \
-                f"Name: {self._name}\n" \
-                f"Class: {self.__class__.__name__}\n" \
-                f"{'_' * 25}\n" \
-                f"Available equipment: \nArmor   -- {self._available_armor}\nWeapons -- {self._available_weapon}\n" \
-                f"{'_' * 25}\n" \
-                f"Inventory: \n{self._inventory}\n" \
-                f"Stats: \n{self._stats}\n" \
-                f"{'*' * 100}\n"
+               f"Race: {self._race}\n" \
+               f"Name: {self._name}\n" \
+               f"Class: {self.__class__.__name__}\n" \
+               f"{'_' * 25}\n" \
+               f"Available equipment: \nArmor   -- {self._available_armor}\nWeapons -- {self._available_weapon}\n" \
+               f"{'_' * 25}\n" \
+               f"Inventory: \n{self._inventory}\n" \
+               f"Stats: \n{self._stats}\n" \
+               f"{'*' * 100}\n"
 
-    # TODO make stats_init func work
-    def stats_init(self): pass
+    def _equip_armor(self, armor):
+        self._inventory['armor'] = armor
+        for stat in NPCConfig._armor_types_dict[armor]:
+            self._stats[stat] += NPCConfig._armor_types_dict[armor][stat]
+
+    def _equip_weapon(self, weapon):
+        self._inventory['weapon'] = weapon
+        for stat in NPCConfig._weapon_types_dict[self.__class__.__name__.lower()][weapon]:
+            self._stats[stat] += NPCConfig._weapon_types_dict[self.__class__.__name__.lower()][weapon][stat]
+
+    def _stats_init(self):
+        armor = self._available_armor[randrange(len(self._available_armor))]
+        weapon = self._available_weapon[randrange(len(self._available_weapon))]
+
+        for stat in NPCConfig._races_stats_dict[self._race]:
+            self._stats[stat] += NPCConfig._races_stats_dict[self._race][stat]
+        self._equip_armor(armor)
+        self._equip_weapon(weapon)
 
     def clone(self,
               race: str = None,
@@ -127,16 +143,18 @@ class NPC(NPCPrototype, NPCConfig):
 
         if not race:
             new._race = NPCConfig._races_list[randrange(len(NPCConfig._races_list))]
-        else: new._race = race
+        else:
+            new._race = race
 
         if not name:
             new._name = NPCConfig._names_dict[new._race][randrange(len(NPCConfig._names_dict[new._race]))]
-        else: new._name = name
+        else:
+            new._name = name
 
         if available_armor: new._available_armor = available_armor
         if available_weapon: new._available_weapon = available_weapon
 
-        new.stats_init()
+        new._stats_init()
 
         return new
 
@@ -144,7 +162,7 @@ class NPC(NPCPrototype, NPCConfig):
 class Warrior(NPC):
     def __init__(self):
         NPC.__init__(self)
-        self._stats['stamina'] = 100
+        self._stats['stamina'] += 100
         self._available_armor = [armor_type for armor_type in NPCConfig._armor_types_dict
                                  if 'light armor' not in armor_type]
         self._available_weapon = [weapon_type for weapon_type in NPCConfig._weapon_types_dict['warrior']]
@@ -153,9 +171,9 @@ class Warrior(NPC):
 class Mage(NPC):
     def __init__(self):
         NPC.__init__(self)
-        self._stats['mana'] = 100
+        self._stats['mana'] += 100
         self._available_armor = [armor_type for armor_type in NPCConfig._armor_types_dict
-                                 if 'light armor' in armor_type],
+                                 if 'light armor' in armor_type]
         self._available_weapon = [weapon_type for weapon_type in NPCConfig._weapon_types_dict['mage']]
 
 
@@ -165,12 +183,10 @@ class NPCFactory:
         self._base_mage = Mage()
 
     def get_random_warrior(self):
-        random_warrior = self._base_warrior.clone()
-        return random_warrior
+        return self._base_warrior.clone()
 
     def get_random_mage(self):
-        random_mage = self._base_mage.clone()
-        return random_mage
+        return self._base_mage.clone()
 
     def test(self):
         for npc in self.get_random_warrior(), self.get_random_mage(): print(npc)

0c337363c429f10e32b7a68c51c48706f3c86eb7
Letiso <letisodianta@gmail.com>
2021-09-28 23:35:24 +0300

    Proxy pattern implemented

--- Structural/proxy.py
+++ Structural/proxy.py
@@ -18,6 +18,7 @@ class VideoLibrary(ABC):
     def delVideo(self, video_to_del: str): pass
 
 
+# Service
 class WebVideoLibrary(VideoLibrary):
     def __init__(self):
         self._videos = {
@@ -44,52 +45,111 @@ class WebVideoLibrary(VideoLibrary):
         for video in self._videos: print(video)
 
     def getVideoInfo(self, query: str) -> None:
-        print(f"\nInformation about {query} video:\n{self._videos[query]['info']}")
+        print(f'\nInformation about "{query}" video:\n{self._videos[query]["info"]}')
 
-    def getVideo(self, query: str) -> None:
+    def getVideo(self, query: str) -> str:
         print(f'\nDownloading queried video "{query}" by the following link:\n{self._videos[query]["link"]}')
+        return f'"{query}" video'
 
     def addVideo(self, new_video: tuple) -> None:
         video_name, video_data = new_video
         self._videos[video_name] = video_data
         print(f'\nNew video "{new_video}" was added to the library')
 
-    def delVideo(self, video_to_del: str):
+    def delVideo(self, video_to_del: str) -> None:
         del self._videos[video_to_del]
         print(f'\nVideo "{video_to_del}" was removed from the library')
 
 
-class WebVideoLibraryProxy(VideoLibrary):
+# Proxies
+class WebVideoLibraryCacheProxy(VideoLibrary):
     def __init__(self):
         self._service = WebVideoLibrary()
+        self._videos_cache = {}
 
     def getVideosList(self):
         self._service.getVideosList()
 
+    def getVideoInfo(self, query: str) -> None:
+        self._service.getVideoInfo(query)
+
+    def getVideo(self, query: str) -> None:
+        if query not in self._videos_cache:
+            self._videos_cache[query] = self._service.getVideo(query)
+        else:
+            print(f'\nGetting cached {self._videos_cache[query]}')
+
+    def addVideo(self, new_video: tuple) -> None:
+        self._service.addVideo(new_video)
+
+    def delVideo(self, video_to_del: str) -> None:
+        if video_to_del in self._videos_cache:
+            del self._videos_cache[video_to_del]
+        self._service.delVideo(video_to_del)
+
+
+def checkAccess(method):
+    def wrapper(self, *args):
+        if self.access: method(self, *args)
+        else:
+            print(self._accessDenied)
+    return wrapper
+
+
+class WebVideoLibrarySecurityProxy(VideoLibrary):
+    def __init__(self):
+        self._service = WebVideoLibraryCacheProxy()
+        # self._service = WebVideoLibrary()
+        self._valid_tokens = ['sk552sr', 'lp476mu', 'zx557jh']
+        self._access = False
+        self._accessDenied = 'Access denied. Please, make sure you have permission'
+
+    @property
+    def access(self):
+        return self._access
+
+    @access.setter
+    def access(self, user_token: str):
+        if user_token in self._valid_tokens:
+            self._access = True
+
+    @checkAccess
+    def getVideosList(self):
+        self._service.getVideosList()
+
+    @checkAccess
     def getVideoInfo(self, query: str):
         self._service.getVideoInfo(query)
 
+    @checkAccess
     def getVideo(self, query: str):
         self._service.getVideo(query)
 
+    @checkAccess
     def addVideo(self, new_video: tuple):
         self._service.addVideo(new_video)
 
+    @checkAccess
     def delVideo(self, video_to_del: str):
         self._service.delVideo(video_to_del)
 
 
+# Client code
 if __name__ == '__main__':
     def client_code(video_lib: VideoLibrary):
-        video_lib.getVideosList()
+        video_lib.access = 'lp476mu'
+        # video_lib.access = 'ld772uo'
 
+        video_lib.getVideosList()
         video_lib.getVideo('trade')
 
         video_lib.addVideo(('coding', {'info': 'Video about coding', 'link': 'http://web_link_at_video_about_coding'}))
         video_lib.getVideoInfo('coding')
 
-        video_lib.delVideo('trade')
+        video_lib.getVideo('trade')
+        video_lib.delVideo('war')
+
         video_lib.getVideosList()
 
 
-    client_code(WebVideoLibraryProxy())
+    client_code(WebVideoLibrarySecurityProxy())

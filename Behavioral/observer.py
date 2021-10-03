922f40a654376dc86e592e3be812550fac68839d
Letiso <letisodianta@gmail.com>
2021-10-03 23:26:17 +0300

    Base observer pattern implemention

---
+++ Behavioral/observer.py
@@ -0,0 +1,100 @@
+from abc import ABC, abstractmethod
+from random import sample, randrange
+
+
+# Publisher abstract class
+class Publisher(ABC):
+    @abstractmethod
+    def subscribe(self, subscriber: 'Subscriber', product_type: str) -> None: pass
+
+    @abstractmethod
+    def unsubscribe(self, subscriber: 'Subscriber', product_type: str) -> None: pass
+
+    @abstractmethod
+    def notify(self, notifying: dict) -> None: pass
+
+
+# Concrete publishers
+class Shop(Publisher):
+    def __init__(self) -> None:
+        self._products: dict = {
+            'phones': {
+                'Meizu': True,
+                'OnePlus': True,
+                'Redmi': True,
+                'Samsung': None
+            },
+            'tv': {
+                'LG': True,
+                'Samsung': True,
+                'Xiaomi': True,
+                'Ozone': None
+            },
+            'notebooks': {
+                'Asus': True,
+                'HP': True,
+                'Lenovo': True,
+                'Predator': None
+            },
+        }
+        self._subscribers = {productType: [] for productType in self._products}
+
+    @property
+    def products(self) -> dict:
+        return self._products
+
+    def update_assortment(self, products: dict) -> None:
+        if not products: return
+
+        for productType in products:
+            self._products[productType].update(products[productType])
+            notifying = {'publisher': self.__class__.__name__,
+                         'message': 'Was appeared a products from your wishlist in our assortment',
+                         'product_type': productType}
+
+            self.notify(notifying)
+
+    def subscribe(self, subscriber: 'Subscriber', product_type: str) -> None:
+        self._subscribers[product_type].append(subscriber)
+
+    def unsubscribe(self, subscriber: 'Subscriber', product_type: str) -> None:
+        del (products := self._subscribers[product_type])[products.index(subscriber)]
+
+    def notify(self, notifying: dict) -> None:
+        for subscriber in self._subscribers[notifying['product_type']]:
+            subscriber.update(notifying)
+
+
+# Subscriber abstract class
+class Subscriber(ABC):
+    @abstractmethod
+    def update(self, notifying: dict) -> None: pass
+
+
+# Concrete subscribers
+class Customer(Subscriber):
+    def __init__(self, wishlist: list):
+        self._wishlist: list = wishlist
+
+    @property
+    def wishlist(self) -> list:
+        return self._wishlist
+
+    def update(self, notifying: dict) -> None:
+        print(f'Taking massage from {notifying["publisher"]}\n'
+              f'Message:\n{notifying["message"]}:\n'
+              f'{notifying["productType"]}')
+
+
+# Client code
+if __name__ == '__main__':
+    def client_code() -> None:
+        shop = Shop()
+        customers = [Customer(sample(sorted(shop.products), randrange(1, 4))) for customer in range(5)]
+
+        for customer in customers:
+            print(customer.wishlist)
+            for productType in customer.wishlist:
+                shop.subscribe(customer, productType)
+
+    client_code()

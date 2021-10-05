f57a14947f9ffaaa8c327135393608ae13be1caa
Letiso <letisodianta@gmail.com>
2021-10-06 01:48:13 +0300

    Visitor pattern implemented

---
+++ Behavioral/visitor.py
@@ -0,0 +1,68 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+
+class Component(ABC):
+    @abstractmethod
+    def accept(self, visitor: 'Visitor'): pass
+
+
+class FirstComponent(Component):
+    def accept(self, visitor: 'Visitor'):
+        visitor.visit_first_component(self)
+
+    def special_first_component_logic(self):
+        print(f"{self.__class__.__name__}: Doing special logic...")
+
+        return 'A'
+
+
+class SecondComponent(Component):
+    def accept(self, visitor: 'Visitor'):
+        visitor.visit_second_component(self)
+
+    def exclusive_second_component_logic(self):
+        print(f"{self.__class__.__name__}: Doing special logic...")
+
+        return 'B'
+
+
+class Visitor:
+    @abstractmethod
+    def visit_first_component(self, component: FirstComponent): pass
+
+    @abstractmethod
+    def visit_second_component(self, component: SecondComponent): pass
+
+
+class FirstVisitor(Visitor):
+    def visit_first_component(self, component: FirstComponent):
+        print(f"{component.special_first_component_logic()} + FirstVisitor additional logic")
+
+    def visit_second_component(self, component: SecondComponent):
+        print(f"{component.exclusive_second_component_logic()} + FirstVisitor additional logic\n")
+
+
+class SecondVisitor(Visitor):
+    def visit_first_component(self, component: FirstComponent):
+        print(f"{component.special_first_component_logic()} + SecondVisitor additional logic")
+
+    def visit_second_component(self, component: SecondComponent):
+        print(f"{component.exclusive_second_component_logic()} + SecondVisitor additional logic\n")
+
+
+def client_code(components: List[Component], visitor: Visitor) -> None:
+    for component in components:
+        component.accept(visitor)
+
+
+if __name__ == '__main__':
+    componentsList = [component() for component in Component.__subclasses__()]
+
+    for string, concrete_visitor in zip(
+            ("The client code works with all visitors via the base Visitor interface:",
+             "It allows the same client code to work with different types of visitors:"),
+            Visitor.__subclasses__()):
+
+        print(f"{'—' * 70}\n{string}\n")
+        client_code(componentsList, concrete_visitor())

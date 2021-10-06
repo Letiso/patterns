2af8bdb36e43ba81bed3be5788f945c28116bde7
Letiso <letisodianta@gmail.com>
2021-10-06 13:58:52 +0300

    Was added type hints to Visitor pattern implementation

--- Behavioral/visitor.py
+++ Behavioral/visitor.py
@@ -4,24 +4,24 @@ from typing import List
 
 class Component(ABC):
     @abstractmethod
-    def accept(self, visitor: 'Visitor'): pass
+    def accept(self, visitor: 'Visitor') -> None: pass
 
 
 class FirstComponent(Component):
-    def accept(self, visitor: 'Visitor'):
+    def accept(self, visitor: 'Visitor') -> None:
         visitor.visit_first_component(self)
 
-    def special_first_component_logic(self):
+    def special_first_component_logic(self) -> str:
         print(f"{self.__class__.__name__}: Doing special logic...")
 
         return 'A'
 
 
 class SecondComponent(Component):
-    def accept(self, visitor: 'Visitor'):
+    def accept(self, visitor: 'Visitor') -> None:
         visitor.visit_second_component(self)
 
-    def exclusive_second_component_logic(self):
+    def exclusive_second_component_logic(self) -> str:
         print(f"{self.__class__.__name__}: Doing special logic...")
 
         return 'B'
@@ -29,25 +29,25 @@ class SecondComponent(Component):
 
 class Visitor:
     @abstractmethod
-    def visit_first_component(self, component: FirstComponent): pass
+    def visit_first_component(self, component: FirstComponent) -> None: pass
 
     @abstractmethod
-    def visit_second_component(self, component: SecondComponent): pass
+    def visit_second_component(self, component: SecondComponent) -> None: pass
 
 
 class FirstVisitor(Visitor):
-    def visit_first_component(self, component: FirstComponent):
+    def visit_first_component(self, component: FirstComponent) -> None:
         print(f"{component.special_first_component_logic()} + FirstVisitor additional logic")
 
-    def visit_second_component(self, component: SecondComponent):
+    def visit_second_component(self, component: SecondComponent) -> None:
         print(f"{component.exclusive_second_component_logic()} + FirstVisitor additional logic\n")
 
 
 class SecondVisitor(Visitor):
-    def visit_first_component(self, component: FirstComponent):
+    def visit_first_component(self, component: FirstComponent) -> None:
         print(f"{component.special_first_component_logic()} + SecondVisitor additional logic")
 
-    def visit_second_component(self, component: SecondComponent):
+    def visit_second_component(self, component: SecondComponent) -> None:
         print(f"{component.exclusive_second_component_logic()} + SecondVisitor additional logic\n")
 
 
